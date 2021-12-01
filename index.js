4c8e7c07c9687d50b8f2835167c5a73df01578b1
Tin Nguyen <hopthucuatin@gmail.com>
2021-12-01 20:59:42 +0700

    Fix express is not installed, add response

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", async (req, res) => {
   try {
     const { url } = req.query;
 
+    if (!url) {
+      return res.status(400).send("url is required");
+    }
+
     const browser = await puppeteer.launch({
       headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -20,14 +24,16 @@ app.get("/", async (req, res) => {
     //get the html
     const html = await page.content();
 
-    console.log(html);
-
     await browser.close();
+
+    res.json(html);
   } catch (error) {
     console.log(error);
+
+    res.json(error);
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(3001, () => {
+  console.log("Server started on port 3001");
 });
